7e9114ee62527025d3ab424727e3a510e4ce1099
baukh789 <182209508@qq.com>
2017-03-11 19:52:38 +0800

    Base类,对方法是否存在进行验证

--- test/Base_test.js
+++ test/Base_test.js
@@ -1,7 +1,51 @@
 'use strict';
 var Base = require('../src/js/Base').default;
-describe('Base', function() {
-	it('验证outLog', function() {
-		expect(Base.outLog('Base outLog')).toBe('Base outLog');
+describe('Base: 验证方法总数', function() {
+	var getPropertyCount = null;
+	beforeEach(function() {
+		getPropertyCount = function(o){
+			var n, count = 0;
+			for(n in o){
+				if(o.hasOwnProperty(n)){
+					count++;
+				}
+			}
+			return count;
+		}
+	});
+	afterEach(function(){
+		getPropertyCount = null;
+	});
+	it('outLog', function() {
+		expect(getPropertyCount(Base)).toBe(9);
+	});
+});
+describe('Base: 验证方法是否存在', function() {
+	it('outLog', function() {
+		expect(Base.outLog).toBeDefined();
+	});
+	it('showTh', function() {
+		expect(Base.showTh).toBeDefined();
+	});
+	it('hideTh', function() {
+		expect(Base.hideTh).toBeDefined();
+	});
+	it('getColTd', function() {
+		expect(Base.getColTd).toBeDefined();
+	});
+	it('initVisible', function() {
+		expect(Base.initVisible).toBeDefined();
+	});
+	it('setAreVisible', function() {
+		expect(Base.setAreVisible).toBeDefined();
+	});
+	it('getTextWidth', function() {
+		expect(Base.getTextWidth).toBeDefined();
+	});
+	it('showLoading', function() {
+		expect(Base.showLoading).toBeDefined();
+	});
+	it('hideLoading', function() {
+		expect(Base.hideLoading).toBeDefined();
 	});
 });
