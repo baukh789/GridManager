feb9ad26822a902d7164c7854551203ee8c45f05
baukh789 <182209508@qq.com>
2018-05-02 09:52:58 +0800

    publish version

--- src/js/Core.js
+++ src/js/Core.js
@@ -205,10 +205,10 @@ class Core {
 						tdNode = jTool(tdTemplate).get(0);
 					} else {
 						tdNode = jTool('<td gm-create="false"></td>').get(0);
-						jTool.type(tdTemplate) === 'element' ? tdNode.appendChild(tdTemplate) : tdNode.innerText = tdTemplate || '';
+						jTool.type(tdTemplate) === 'element' ? tdNode.appendChild(tdTemplate) : tdNode.innerHTML = tdTemplate || '';
 					}
 
-                    // td 文本对齐方向
+					// td 文本对齐方向
 					col.align && tdNode.setAttribute('align', col.align);
 					trNode.appendChild(tdNode);
 				});
