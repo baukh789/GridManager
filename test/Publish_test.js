7f05879bcefeed9d715a539788b90fbd1e7f04d1
baukh789 <182209508@qq.com>
2018-08-06 19:24:08 +0800

    update test

--- test/Publish_test.js
+++ test/Publish_test.js
@@ -301,7 +301,7 @@ describe('PublishMethod 非init方法验证', function() {
         });
 
         it('验证百分比', function () {
-            overflowX = PublishMethod.resetLayout(table, '100%', '100%');
+            overflowX = PublishMethod.resetLayout(table, '90%', '100%');
             expect(overflowX).toBe('hidden');
             style = $tableWrap.get(0).style;
             expect(style.width).toBe('calc(100%)');
