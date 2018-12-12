dc62fa04d6f466a3ea5eaa1ac83e33e45e61fe46
baukh789 <182209508@qq.com>
2018-12-12 13:56:26 +0800

    清除注释

--- src/js/index.js
+++ src/js/index.js
@@ -68,7 +68,6 @@ import { PublishMethod, publishMethodArray } from './Publish';
             let thisWidth = window.getComputedStyle(this).width;
             if (thisWidth.indexOf('px') !== -1) {
                 clearInterval(siv);
-                console.log(this, window.getComputedStyle(this).width);
                 return PublishMethod[name](this, arg, callback, condition) || this;
             }
         }, 50);
