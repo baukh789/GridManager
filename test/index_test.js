4bdee28eca31e8b0fbbcb7ba59cbc354fe0fa1a8
baukh789 <182209508@qq.com>
2019-03-31 22:42:01 +0800

    add parse test

--- test/index_test.js
+++ test/index_test.js
@@ -1,4 +1,5 @@
 import './constants_test';
+import './parse_test';
 import './base_fn_test';
 import './Settings_test';
 import './Store_test';
---
+++ test/parse_test.js
@@ -0,0 +1,46 @@
+import { parseTpl, trimTpl } from '../src/common/parse';
+
+
+describe('trimTpl', () => {
+    it('基础验证', () => {
+        let str = `<table class="dreamland-table {{vm.tableClassName}}">
+                        <tbody>
+                            {{vm.tbodyHtml}}
+                        </tbody>
+                    </table>`;
+        let trimStr = '<table class="dreamland-table {{vm.tableClassName}}"><tbody>{{vm.tbodyHtml}}</tbody></table>';
+        expect(trimTpl(str)).toBe(trimStr);
+        str = null;
+        trimStr = null;
+    });
+});
+
+describe('parseTpl', () => {
+    it('基础验证', () => {
+        let str = `<table class="dreamland-table {{vm.tableClassName}}">
+                        <tbody>
+                            {{vm.tbodyHtml}}
+                        </tbody>
+                    </table>`;
+        let trimStr = '<table class="dreamland-table kouzi"><tbody>like kouzi</tbody></table>';
+        let target = {};
+        let key = 'testDecorator';
+        let descriptor = {
+            configurable: true,
+            enumerable: false,
+            writable: true,
+            value: params => {
+                return {
+                    tableClassName: params.name,
+                    tbodyHtml: params.html
+                };
+            }
+        };
+        let params = {
+            name: 'kouzi',
+            html: 'like kouzi'
+        };
+        parseTpl(str)(target, key, descriptor);
+        expect(descriptor.value(params)).toBe(trimStr);
+    });
+});
