de59deb228daa08e9f34b0fa296f368322f46329
baukh789 <182209508@qq.com>
2020-02-10 18:12:22 +0800

    optimization code

--- src/jTool/Offset.js
+++ src/jTool/Offset.js
@@ -9,75 +9,73 @@
  * #Offset003:	原先offset()方法使用的是递归形式: 递增寻找position !== static的父级节点offsetTop或offsetLeft的值, 直到node.nodeType !== 1的时候停止
  * 				这种方法正常情况下没什么问题, 但是当body的position !== static时, 所计算的offset值将不包含scroll卷去的值
  * */
-import { getStyle, isWindow } from './utils';
+import { getStyle, isNumber } from './utils';
 
+/**
+ * 根据参数对位置操作进行get,set分类操作
+ * @param node
+ * @param value
+ * @param type
+ * @returns {undefined|number|*}
+ */
+const scrollFN = (node, value, type) => {
+    // scroll 对应 element 属性
+    const scrollAttr = {
+        top: 'scrollTop',
+        left: 'scrollLeft'
+    }[type];
+
+    // 转换: node === document
+    if (node.nodeType === 9) {
+        node = node.body;
+    }
+
+    // setter
+    if (isNumber(value)) {
+        node[scrollAttr] = value;
+        return this;
+    }
+
+    // getter
+    return node[scrollAttr];
+};
 export default {
 
 	// #Offset003
 	// 获取匹配元素在当前视口的相对偏移
 	offset: function () {
-		let offest = {
+		let position = {
 			top: 0,
 			left: 0
 		};
 		const node = this.DOMList[0];
+
 		// #Offset001
 		// 当前为IE11以下, 直接返回{top: 0, left: 0}
 		if (!node.getClientRects().length) {
-			return offest;
+			return position;
 		}
+
 		// 当前DOM节点的 display === 'node' 时, 直接返回{top: 0, left: 0}
 		if (getStyle(node, 'display') === 'none') {
-			return offest;
+			return position;
 		}
+
 		// #Offset002
-		offest = node.getBoundingClientRect();
-		var docElement = node.ownerDocument.documentElement;
+        position = node.getBoundingClientRect();
+		const docElement = node.ownerDocument.documentElement;
 		return {
-			top: offest.top + window.pageYOffset - docElement.clientTop,
-			left: offest.left + window.pageXOffset - docElement.clientLeft
+			top: position.top + window.pageYOffset - docElement.clientTop,
+			left: position.left + window.pageXOffset - docElement.clientLeft
 		};
 
 	},
 	// 获取|设置 匹配元素相对滚动条顶部的偏移 value is number
 	scrollTop: function (value) {
-		return this.scrollFN(value, 'top');
+		return scrollFN(this.DOMList[0], value, 'top');
 	},
 	// 获取|设置 匹配元素相对滚动条左部的偏移 value is number
 	scrollLeft: function (value) {
-		return this.scrollFN(value, 'left');
-	},
-	// 根据参数对位置操作进行get,set分类操作
-	scrollFN: function (value, type) {
-		const node = this.DOMList[0];
-		// setter
-		if (value || value === 0) {
-			this.setScrollFN(node, type, value);
-			return this;
-		} else { // getter
-			return this.getScrollFN(node, type);
-		}
-	},
-	// 根据元素的不同对滚动轴进行获取
-	getScrollFN: function (node, type) {
-		// node => window
-		if (isWindow(node)) {
-			return type === 'top' ? node.pageYOffset : node.pageXOffset;
-		} else if (node.nodeType === 9) { // node => document
-			return type === 'top' ? node.body.scrollTop : node.body.scrollLeft;
-		} else if (node.nodeType === 1) { // node => element
-			return type === 'top' ? node.scrollTop : node.scrollLeft;
-		}
-	},
-	// 根据元素的不同对滚动轴进行设置
-	setScrollFN: function (node, type, value) {
-		// node => window
-		if (isWindow(node)) {
-			type === 'top' ? node.document.body.scrollTop = value : node.document.body.scrollLeft = value;
-		} else if (node.nodeType === 9) { // node => document
-			type === 'top' ? node.body.scrollTop = value : node.body.scrollLeft = value;
-		} else if (node.nodeType === 1) { // node => element
-			type === 'top' ? node.scrollTop = value : node.scrollLeft = value;
-		}
+		return scrollFN(this.DOMList[0], value, 'left');
 	}
 };
