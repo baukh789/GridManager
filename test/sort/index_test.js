669263d0dacb1b7d220321e7e915c4275d68af87
写个程序换个饼 <182209508@qq.com>
2022-01-11 16:49:15 +0800

    sort 生成html简化

--- test/sort/index_test.js
+++ test/sort/index_test.js
@@ -9,12 +9,26 @@ describe('sort', () => {
             htmlStr = null;
         });
 
-        it('执行验证', () => {
+        it('执行验证: 无排序', () => {
             htmlStr = `<div class="gm-sorting-action">
                         <i class="sa-icon sa-up gm-icon gm-icon-up"></i>
                         <i class="sa-icon sa-down gm-icon gm-icon-down"></i>
                     </div>`.replace(/\s/g, '');
-            expect(sort.createHtml().replace(/\s/g, '')).toBe(htmlStr);
+            expect(sort.createHtml({type: '', sortUpText: 'ASC', sortDownText: 'DESC'}).replace(/\s/g, '')).toBe(htmlStr);
         });
+		it('执行验证: 向上排序', () => {
+			htmlStr = `<div class="gm-sorting-action sorting-up">
+                        <i class="sa-icon sa-up gm-icon gm-icon-up"></i>
+                        <i class="sa-icon sa-down gm-icon gm-icon-down"></i>
+                    </div>`.replace(/\s/g, '');
+			expect(sort.createHtml({type: 'ASC', sortUpText: 'ASC', sortDownText: 'DESC'}).replace(/\s/g, '')).toBe(htmlStr);
+		});
+		it('执行验证: 向下排序', () => {
+			htmlStr = `<div class="gm-sorting-action sorting-down">
+                        <i class="sa-icon sa-up gm-icon gm-icon-up"></i>
+                        <i class="sa-icon sa-down gm-icon gm-icon-down"></i>
+                    </div>`.replace(/\s/g, '');
+			expect(sort.createHtml({type: 'DESC', sortUpText: 'ASC', sortDownText: 'DESC'}).replace(/\s/g, '')).toBe(htmlStr);
+		});
     });
 });
